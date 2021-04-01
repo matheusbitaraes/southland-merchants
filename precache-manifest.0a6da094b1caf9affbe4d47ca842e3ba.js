d8d0a35f435930cc045e4db1724628a6bd844aec
matheusbitaraes <matheusbitaraesdenovaes@gmail.com>
2021-03-31 23:19:27 -0300

    Updates

---
+++ precache-manifest.0a6da094b1caf9affbe4d47ca842e3ba.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045ebd8d60d1a1cba32860aca8c95a39",
+    "url": "/southland-merchants/index.html"
+  },
+  {
+    "revision": "4b8e90eabd79be8bd63c",
+    "url": "/southland-merchants/static/css/main.4f92a863.chunk.css"
+  },
+  {
+    "revision": "93e6f7fd2caf78371831",
+    "url": "/southland-merchants/static/js/2.cb65fb59.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/southland-merchants/static/js/2.cb65fb59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8e90eabd79be8bd63c",
+    "url": "/southland-merchants/static/js/main.59053c6d.chunk.js"
+  },
+  {
+    "revision": "106d147996ef4eed31a3",
+    "url": "/southland-merchants/static/js/runtime-main.a4a36413.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/southland-merchants/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/southland-merchants/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/southland-merchants/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/southland-merchants/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/southland-merchants/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/southland-merchants/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/southland-merchants/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/southland-merchants/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/southland-merchants/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/southland-merchants/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/southland-merchants/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/southland-merchants/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/southland-merchants/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/southland-merchants/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
